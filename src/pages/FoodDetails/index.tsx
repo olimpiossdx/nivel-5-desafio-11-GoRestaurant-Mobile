9ec4098369683c59f54b4b78acdffb2426a7d334
Olimpio Pimenta <olimpiodx@gmail.com>
2021-02-14 02:32:42 -0300

    Realizar um pedido

--- src/pages/FoodDetails/index.tsx
+++ src/pages/FoodDetails/index.tsx
@@ -61,7 +61,7 @@ interface Food {
 }
 
 const FoodDetails: React.FC = () => {
-  const [food, setFood] = useState({} as Food);
+  const [food, setFood] = useState<Food>({} as Food);
   const [extras, setExtras] = useState<Extra[]>([]);
   const [isFavorite, setIsFavorite] = useState(false);
   const [foodQuantity, setFoodQuantity] = useState(1);
@@ -70,48 +70,107 @@ const FoodDetails: React.FC = () => {
   const route = useRoute();
 
   const routeParams = route.params as Params;
+  const { id } = routeParams;
 
   useEffect(() => {
-    async function loadFood(): Promise<void> {
-      // Load a specific food with extras based on routeParams id
+    async function loadFavorite(): Promise<void> {
+      await api.get<Food[]>(`favorites`).then(response => {
+        response.data.filter(favorite => {
+          if (favorite.id === id) {
+            setIsFavorite(true);
+          }
+        });
+      });
     }
 
-    loadFood();
+    async function loadFood(): Promise<void> {
+      const responseFood = await api.get<Food>(`foods/${id}`);
+      const formatedExtras = responseFood.data.extras.map(extra => {
+        extra.quantity = 0;
+        return extra;
+      });
+      responseFood.data.formattedPrice = formatValue(responseFood.data.price);
+      setFood(responseFood.data);
+      setExtras(formatedExtras);
+    }
+
+    if (routeParams.id) {
+      loadFood();
+      loadFavorite();
+    }
   }, [routeParams]);
 
   function handleIncrementExtra(id: number): void {
+    setExtras(extras => {
+      return extras.map(extra => {
+        if (extra.id === id) {
+          extra.quantity += 1;
+        }
+        return extra;
+      });
+    })
     // Increment extra quantity
   }
 
   function handleDecrementExtra(id: number): void {
+    setExtras(extras => {
+      return extras.map(extra => {
+        if (extra.id === id && extra.quantity >= 1) {
+          extra.quantity -= 1;
+        }
+        return extra;
+      });
+    })
     // Decrement extra quantity
   }
 
   function handleIncrementFood(): void {
-    // Increment food quantity
+    setFoodQuantity(foodQuantity => foodQuantity += 1);
   }
 
   function handleDecrementFood(): void {
-    // Decrement food quantity
+    setFoodQuantity(foodQuantity => {
+      if (foodQuantity > 1) {
+        foodQuantity -= 1
+      }
+      return foodQuantity;
+    });
   }
 
   const toggleFavorite = useCallback(() => {
-    // Toggle if food is favorite or not
-  }, [isFavorite, food]);
+    if (isFavorite) {
+      api.delete(`favorites/${food.id}`).then(response => {
+        setIsFavorite(!isFavorite);
+      });
+    } else {
+      setFood(state => {
+        const { extras, formattedPrice, ...favoriteFood } = state;
+        api.post('favorites', favoriteFood).then(response => {
+          setIsFavorite(!isFavorite);
+        });
+        return state;
+      });
+    }
+  }, [isFavorite]);
 
   const cartTotal = useMemo(() => {
-    // Calculate cartTotal
+    let totalAmount = food.price * foodQuantity;
+    if (food.extras) {
+      totalAmount += food.extras.reduce((soma, extra) => soma += extra.value * extra.quantity, 0);
+    }
+    return formatValue(totalAmount);
   }, [extras, food, foodQuantity]);
 
   async function handleFinishOrder(): Promise<void> {
     // Finish the order and save on the API
+    const { formattedPrice, id, ...order } = food;
+    api.post('orders', order).then(response => {
+      console.log('response.data', response.data);
+    });
   }
 
   // Calculate the correct icon name
-  const favoriteIconName = useMemo(
-    () => (isFavorite ? 'favorite' : 'favorite-border'),
-    [isFavorite],
-  );
+  const favoriteIconName = useMemo(() => (isFavorite ? 'favorite' : 'favorite-border'), [isFavorite]);
 
   useLayoutEffect(() => {
     // Add the favorite icon on the right of the header bar
@@ -135,12 +194,7 @@ const FoodDetails: React.FC = () => {
         <FoodsContainer>
           <Food>
             <FoodImageContainer>
-              <Image
-                style={{ width: 327, height: 183 }}
-                source={{
-                  uri: food.image_url,
-                }}
-              />
+              <Image style={{ width: 327, height: 183 }} source={{ uri: food.image_url }} />
             </FoodImageContainer>
             <FoodContent>
               <FoodTitle>{food.name}</FoodTitle>
